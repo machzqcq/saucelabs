0005b34bb87953699961a2189b3b79faaed67d43
unknown <bhc480@MUSAVA171980.cof.ds.capitalone.com>
2014-07-22 23:16:26 -0400

    parsing and extracting browser version and platform

---
+++ lib/saucelabs/parsed_values.rb
@@ -0,0 +1,54 @@
+module SauceModule
+  module ParsedValues
+
+    def extract_values_from(browser_string)
+      browser = extract_browser(browser_string).to_sym
+      version = extract_version(browser_string)
+      platform = extract_platform(browser_string)
+      return browser,version,platform
+    end
+
+    private
+
+    def extract_browser(value)
+      browser = value.split(/\d*/)[0]
+      browser = browser.split('|')[0] if browser.include? '|'
+      browser
+    end
+
+    def extract_version(value)
+      value = value.split('|') if value.include? '|'
+      regexp_to_match = /\d{1,}/
+      if (not regexp_to_match.match(value).nil?)
+        version = regexp_to_match.match(value)[0]
+      else
+        version = ''
+      end
+      version
+    end
+
+    def extract_platform(value)
+      platform = value.split('|')[1]
+      platform
+    end
+
+    def sauce_platform_hash
+      @sauce_platforms = Hash.new
+      @sauce_platforms[:winxp] = "Windows XP"
+      @sauce_platforms["osx10.9".to_sym] = "OS X 10.9"
+      @sauce_platforms["osx10.6".to_sym] = "OS X 10.6"
+      @sauce_platforms["osx10.8".to_sym] = "OS X 10.8"
+      @sauce_platforms[:linux] = "Linux"
+      @sauce_platforms["windows8.1".to_sym] = "Windows 8.1"
+      @sauce_platforms["windows8".to_sym] = "Windows 8"
+      @sauce_platforms["windows7".to_sym] = "Windows 7"
+    end
+
+    def sauce_device_hash
+      @sauce_devices = Hash.new
+      @sauce_devices[:iphone] = "iPhone"
+      @sauce_devices[:android] = "Android"
+    end
+
+  end
+end
