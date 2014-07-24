253a92d331b054a8021a549923ab81e54ccdbdfa
unknown <bhc480@MUSAVA171980.cof.ds.capitalone.com>
2014-07-23 20:09:02 -0400

    defining the scaffolding

--- lib/saucelabs.rb
+++ lib/saucelabs.rb
@@ -2,6 +2,25 @@ require 'saucelabs/sauce_browser_factory'
 require 'saucelabs/parsed_values'
 
 module SauceLabs
+  attr_accessor :factory
+
+  def self.sauce_factory
+      factory.browser
+  end
+
+  def self.browser(browser = :chrome, browser_options={})
+    SauceBrowserFactory.new(browser,browser_options)
+  end
+
+  def self.conf
+    yield
+  end
+
+  private
+
+  def self.factory
+    @factory = SauceBrowserFactory.new unless factory
+  end
 
   
 
