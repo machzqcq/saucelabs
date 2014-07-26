d52217e853b722f30ebd8da8f5bf71263bf1f477
unknown <bhc480@MUSAVA171980.cof.ds.capitalone.com>
2014-07-26 19:57:54 -0400

    browser factory updated

--- lib/saucelabs/sauce_browser_factory.rb
+++ lib/saucelabs/sauce_browser_factory.rb
@@ -1,20 +1,24 @@
 require_relative 'parsed_values'
+require 'selenium-webdriver'
+require 'watir-webdriver'
 
 module SauceLabs
   class SauceBrowserFactory
     include ParsedValues
 
-    attr_accessor :url,:browser_options
+    attr_accessor :url,:options,:persistent_http
 
     def watir_browser(browser,browser_options)
       target,options = browser_caps(browser,browser_options)
       create_watir_browser(target,options)
     end
 
-    def selenium_browser(browser,browser_options)
+    def selenium_driver(browser,browser_options)
       target,options = browser_caps(browser,browser_options)
-      create_selenium_browser(target,options)
+      create_selenium_driver(target,options)
     end
+
+    private
 
     def create_watir_browser(target,options)
       if options.empty?
@@ -24,7 +28,7 @@ module SauceLabs
       end
     end
 
-    def create_selenium_browser(target,options)
+    def create_selenium_driver(target,options)
       if options.empty?
         Selenium::WebDriver.for target
       else
@@ -32,33 +36,38 @@ module SauceLabs
       end
     end
 
-    def capabilities(browser, version, platform)
+    def browser_caps(browser,browser_options)
+      target = (browser.to_sym if ENV['BROWSER'].nil? or ENV['browser'].empty?) || (ENV['BROWSER'].to_sym)
+      browser,version,platform,device = extract_values_from(target)
+      options = {}
+      options.merge! browser_options
+      caps = capabilities(browser,version,platform,device)
+      options[:url] = url if url
+      if options.include? :url
+        browser = :remote
+        options[:desired_capabilities] = caps
+      end
+      options[:http_client] = http_client if persistent_http or options.delete(:persistent_http)
+      return browser,options
+    end
+
+
+    def capabilities(browser, version, platform,device={})
       capabilities = Selenium::WebDriver::Remote::Capabilities.send browser
-      capabilities.version = version unless version.nil?
-      capabilities.platform = platform unless platform.nil?
+      capabilities.version = version unless version.nil? or version.empty?
+      capabilities.platform = platform unless platform.nil? or platform.empty?
+      capabilities.device = device unless device.nil? or device.empty?
       capabilities
     end
 
-    def self.http_client
-      client = Selenium::WebDriver::Remote::Http::Default.new
-      client.timeout = 180
-      client
+    def http_client
+     client = Selenium::WebDriver::Remote::Http::Persistent.new
+     #client.proxy_uri="http://abc"
+     #client.read_timeout = 180
+     #client.socket_options = {}
+     client
     end
 
-    def browser_caps(browser,browser_options)
-      browser = ENV['browser'].to_sym if ENV['browser']
-      browser,version,platform = extract_values_from(browser)
-      options = {}
-      options.merge! browser_options
-      caps = capabilities(browser,version,platform)
-      options[:url] = url
-      if options.include? :url
-        target = :remote
-        options[:desired_capabilities] = caps
-      end
-      options[:http_client] = '' unless http_client
-      return target,options
-    end
 
   end
 
