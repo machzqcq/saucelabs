2be13316ea7ef450d6a65734049739fb25f6f5cd
unknown <bhc480@MUSAVA171980.cof.ds.capitalone.com>
2014-07-26 19:57:18 -0400

    updated the parsing logic

--- lib/saucelabs/parsed_values.rb
+++ lib/saucelabs/parsed_values.rb
@@ -5,7 +5,8 @@ module SauceLabs
       browser = extract_browser(browser_string).to_sym
       version = extract_version(browser_string)
       platform = extract_platform(browser_string)
-      return browser,version,platform
+      device = extract_device(browser_string)
+      return browser,version,platform,device
     end
 
     private
@@ -17,7 +18,7 @@ module SauceLabs
     end
 
     def extract_version(value)
-      value = value.to_s.split('|') if value.to_s.include? '|'
+      value = value.to_s.split('|')[0] if value.to_s.include? '|'
       regexp_to_match = /\d{1,}/
       if (not regexp_to_match.match(value).nil?)
         version = regexp_to_match.match(value)[0]
@@ -28,26 +29,33 @@ module SauceLabs
     end
 
     def extract_platform(value)
-      platform = value.to_s.split('|')[1]
-      platform
+      platform = value.to_s.split('|')[1] if value.to_s.include? '|'
+      sauce_platforms
+      @sauce_platforms[platform] if not platform.nil?
     end
 
-    def sauce_platform_hash
+    def extract_device(value)
+      device = value.to_s.split('|')[2] if value.to_s.include? '|'
+      sauce_devices
+      @sauce_devices[device] if not device.nil?
+    end
+
+    def sauce_platforms
       @sauce_platforms = Hash.new
-      @sauce_platforms[:winxp] = "Windows XP"
-      @sauce_platforms["osx10.9".to_sym] = "OS X 10.9"
-      @sauce_platforms["osx10.6".to_sym] = "OS X 10.6"
-      @sauce_platforms["osx10.8".to_sym] = "OS X 10.8"
-      @sauce_platforms[:linux] = "Linux"
-      @sauce_platforms["windows8.1".to_sym] = "Windows 8.1"
-      @sauce_platforms["windows8".to_sym] = "Windows 8"
-      @sauce_platforms["windows7".to_sym] = "Windows 7"
+      @sauce_platforms["windowsxp"] = "Windows XP"
+      @sauce_platforms["osx109"] = "OS X 10.9"
+      @sauce_platforms["osx106"] = "OS X 10.6"
+      @sauce_platforms["osx108"] = "OS X 10.8"
+      @sauce_platforms["linux"] = "Linux"
+      @sauce_platforms["windows81"] = "Windows 8.1"
+      @sauce_platforms["windows8"] = "Windows 8"
+      @sauce_platforms["windows7"] = "Windows 7"
     end
 
-    def sauce_device_hash
+    def sauce_devices
       @sauce_devices = Hash.new
-      @sauce_devices[:iphone] = "iPhone"
-      @sauce_devices[:android] = "Android"
+      @sauce_devices["iphone"] = "iPhone"
+      @sauce_devices["android"] = "Android"
     end
 
   end
