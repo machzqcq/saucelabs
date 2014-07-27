fc8317102aea476838e8807104ed7d836ddfe43b
unknown <bhc480@MUSAVA171980.cof.ds.capitalone.com>
2014-07-26 20:38:46 -0400

    documentation added for parser

--- lib/saucelabs/parsed_values.rb
+++ lib/saucelabs/parsed_values.rb
@@ -1,6 +1,16 @@
 module SauceLabs
   module ParsedValues
 
+    #
+    # Extracts browser, version, platform, device from the browser string
+    #
+    # @example
+    #   extract_values_from(:'safari5|linux|iphone') will extract
+    # browser = safari, version=5, platform=Linux, device=iPhone
+    #
+    # @param  [String] the browser string passed into the method
+    # @return [String,String,String,String] browser, version, platform and device
+    #
     def extract_values_from(browser_string)
       browser = extract_browser(browser_string).to_sym
       version = extract_version(browser_string)
@@ -11,12 +21,32 @@ module SauceLabs
 
     private
 
+    #
+    # Extracts browser from browser string
+    #
+    # @example
+    #   extract_values_from(:'safari5|linux|iphone') will extract
+    # browser = safari
+    #
+    # @param  [String] the browser string passed into the method
+    # @return [String] the browser value
+    #
     def extract_browser(value)
       browser = value.to_s.split(/\d+/)[0]
       browser = browser.to_s.split('|')[0] if browser.to_s.include? '|'
       browser
     end
 
+    #
+    # Extracts version from browser string
+    #
+    # @example
+    #   extract_values_from(:'safari5|linux|iphone') will extract
+    # browser = safari
+    #
+    # @param  [String] the browser string passed into the method
+    # @return [String] the version value
+    #
     def extract_version(value)
       value = value.to_s.split('|')[0] if value.to_s.include? '|'
       regexp_to_match = /\d{1,}/
@@ -28,17 +58,41 @@ module SauceLabs
       version
     end
 
+    #
+    # Extracts platform from browser string
+    #
+    # @example
+    #   extract_values_from(:'safari5|linux|iphone') will extract
+    # browser = safari
+    #
+    # @param  [String] the browser string passed into the method
+    # @return [String] the platform value
+    #
     def extract_platform(value)
       platform = value.to_s.split('|')[1] if value.to_s.include? '|'
       sauce_platforms
       @sauce_platforms[platform] if not platform.nil?
     end
 
+    #
+    # Extracts device from browser string
+    #
+    # @example
+    #   extract_values_from(:'safari5|linux|iphone') will extract
+    # browser = safari
+    #
+    # @param  [String] the browser string passed into the method
+    # @return [String] the device value
+    #
+
     def extract_device(value)
       device = value.to_s.split('|')[2] if value.to_s.include? '|'
       sauce_devices
       @sauce_devices[device] if not device.nil?
     end
+
+    #
+    # Defines the platforms available on Sauce Labs as a Hash
 
     def sauce_platforms
       @sauce_platforms = Hash.new
@@ -51,6 +105,10 @@ module SauceLabs
       @sauce_platforms["windows8"] = "Windows 8"
       @sauce_platforms["windows7"] = "Windows 7"
     end
+
+    #
+    # Defines the devices available on Sauce Labs as a Hash
+
 
     def sauce_devices
       @sauce_devices = Hash.new
