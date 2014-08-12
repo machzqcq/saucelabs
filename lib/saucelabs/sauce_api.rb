0cc53b541ae3f40aee4f18020ba93dbd3bac87d6
Pradeep <macharla.pradeep.kumar@gmail.com>
2014-08-11 21:05:06 -0400

    sauce api

---
+++ lib/saucelabs/sauce_api.rb
@@ -0,0 +1,29 @@
+require "net/http"
+require "net/https"
+
+module SauceLabs
+  module RestApiBuilder
+  
+  def post(body)
+  
+  end
+  
+  def puts(body)
+  
+  end
+  
+  def get(url)
+  
+  end
+  
+  def delete(url)
+  
+  end
+  
+  def sauce_credentials
+    
+  end
+  
+  
+  end
+end
